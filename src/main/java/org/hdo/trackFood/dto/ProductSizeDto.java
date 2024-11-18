9daa78f386fef9246d22acd343e9c44b3d05e772
HalomkoDmytro <galomko.dmytro@gmail.com>
2024-11-18 18:30:57 +0200

    crud logic

--- src/main/java/org/hdo/trackFood/dto/ProductSizeDto.java
+++ src/main/java/org/hdo/trackFood/dto/ProductSizeDto.java
@@ -2,5 +2,6 @@ package org.hdo.trackFood.dto;
 
 public record ProductSizeDto(Long id,
                              Long gr,
+                             Long dishId,
                              ProductDto productDto) {
 }
--- src/main/java/org/hdo/trackFood/service/ProductSizeService.java
+++ src/main/java/org/hdo/trackFood/service/ProductSizeService.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.hdo.trackFood.converter.ProductConverter;
 import org.hdo.trackFood.converter.ProductSizeConverter;
 import org.hdo.trackFood.dto.ProductSizeDto;
+import org.hdo.trackFood.model.Dish;
 import org.hdo.trackFood.model.ProductSize;
+import org.hdo.trackFood.repository.DishRepository;
 import org.hdo.trackFood.repository.ProductSizeRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,15 +17,19 @@ import java.util.List;
 public class ProductSizeService {
 
     private ProductSizeRepository productSizeRepository;
+    private DishRepository dishRepository;
 
     public ProductSize update(ProductSizeDto dto) {
         ProductSize productSize;
         if (dto.id() == null) {
             productSize = ProductSizeConverter.convert(dto);
         } else {
+            Dish dish = dishRepository.findById(dto.dishId())
+                    .orElse(null);
+
             productSize = ProductSize.builder()
                     .gr(dto.gr())
-//                    .dish() // todo
+                    .dish(dish)
                     .product(ProductConverter.convert(dto.productDto()))
                     .build();
         }
